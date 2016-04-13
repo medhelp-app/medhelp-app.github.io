eb88300d7fbb2558a3236446a43b427ebfef2108
andrelucasmachadolins <andrelucas01@hotmail.com>
2016-04-13 15:23:50 -0300

    Menu lateral para mobile

--- medhelp/js/controller/homeCtrl.js
+++ medhelp/js/controller/homeCtrl.js
@@ -1,4 +1,22 @@
 app.controller("HomeCtrl", function($scope, $http, $sessionStorage){
-	$('.slider').slider({full_width: true,indicators: false});
 	$(".button-collapse").sideNav();
+	$('.slider').slider({full_width: true,indicators: false});
+	
+	$scope.login = function(user){
+		/*$http({
+		    method: "post",
+		    url: "http://104.236.112.162/api/users",
+		    data: {
+		    	name:"Andre Lucas1",
+		    	email:"andrelucas02	@hotmail.com",
+		    	rePassword: "olegario",
+		    	password:"olegario"
+		    }
+		}).success(function(data){
+		  	console.log(data);
+		}).error(function(data){
+			console.log(data);
+		});*/
+	}
+
 });
