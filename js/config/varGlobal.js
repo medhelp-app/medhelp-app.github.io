a9fa771f7b661450e2f6c00288c9e2a497b28037
andrelucasmachadolins <andrelucas01@hotmail.com>
2016-05-19 00:29:52 -0300

    a

--- js/config/varGlobal.js
+++
@@ -1,5 +0,0 @@
-//var url = "http://apienergy.solarlifenergy.com/";
-//var image = "http://apienergy.solarlifenergy.com/";
-
-// var url = "http://localhost:8080/solarenergy/API/";
-// var image = "http://localhost:8080/solarenergy/API/";
