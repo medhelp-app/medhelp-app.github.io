c160aa0b8bfca0d33473f83dd1b5f30415baf3ac
Paulo Menezes <phgm@cesar.org.br>
2016-06-09 09:41:56 -0300

    Mark appointments

--- js/controller/doctorCtrl.js
+++ js/controller/doctorCtrl.js
@@ -7,10 +7,9 @@ app.controller("DoctorCtrl", function($scope, $http, $cookies, $routeParams, $md
 
 	$http.get(API_URL + 'doctors/' + $routeParams.id, config).then(function (data) {
 		$scope.doctor = data.data;
-		console.log(data);
 	}, function (error) {
 		console.log(error);
-	})
+	});
 
 	function load () {
 		$http.get(API_URL + 'doctors/' + $routeParams.id + '/opinions', config).then(function (data) {
@@ -57,7 +56,6 @@ app.controller("DoctorCtrl", function($scope, $http, $cookies, $routeParams, $md
 
 			$scope.total = total;
 			$scope.opinions = data.data;
-			console.log(total);
 		}, function (error) {
 			console.log(error);
 		});
@@ -97,8 +95,6 @@ app.controller("DoctorCtrl", function($scope, $http, $cookies, $routeParams, $md
 						$scope.add.generalRating = ($scope.add.punctualityRating + $scope.add.attentionRating + $scope.add.installationRating) / 3;
 
 						$http.post(API_URL + 'doctors/' + $routeParams.id + '/opinions', $scope.add, config).then(function (data) {
-							console.log(data);
-
 							load();
 
 							$mdDialog.hide();
@@ -118,4 +114,93 @@ app.controller("DoctorCtrl", function($scope, $http, $cookies, $routeParams, $md
 				fullscreen: $mdMedia('md')
 			});
 	};
+
+	$scope.availability = [];
+	$http.get(API_URL + 'doctors/' + $routeParams.id + '/availability', config).then(function (data) {
+		$scope.availability = data.data;
+	}, function (error) {
+		console.log(error);
+	});
+
+	$scope.appointment = function (ev) {
+		var availability = $scope.availability;
+		
+		$mdDialog.show({
+				controller: function ($scope) {
+					$scope.add = {
+						date: null,
+						availabilityId: 0,
+						patientId: $cookies.get('user'),
+						doctorId: $routeParams.id
+					};
+
+					var days = {
+						'sunday': 0, 'monday' : 1, 'tuesday': 2, 'wednesday': 3, 
+						'thursday': 4, 'friday': 5, 'saturday': 6
+					};
+
+					var doctorDays = [];
+
+					for (var i = 0; i < availability.length; i++) {
+						doctorDays[days[availability[i].weekday]] = true;
+					}
+
+					$scope.minDate = new Date();
+					$scope.appointments = function (date) {
+						var day = date.getDay();
+
+						var r = false;
+						for (var i = 0; i < doctorDays.length; i++) {
+							if (doctorDays[i] == day)
+								r = true;
+						}
+
+    					return r; // day === 1 || day === 2;
+					}
+
+					$scope.hours = [];
+					$scope.selectDate = function () {
+						var d = new Date($scope.add.date);
+						var day = d.getDay();
+
+						for (var i = 0; i < availability.length; i++) {
+							console.log(day, days[availability[i].weekday], $scope.add.date);
+							if (day == days[availability[i].weekday]) 
+								$scope.hours.push(availability[i]);
+						}
+					}
+
+					$scope.availability = availability;
+
+					$scope.save = function () {
+						$http.post(API_URL + 'doctors/' + $routeParams.id + '/appointments', $scope.add, config).then(function (data) {
+							console.log(data);
+
+							$mdDialog.show(
+								$mdDialog.alert()
+									.parent(angular.element(document.querySelector('#main')))
+									.clickOutsideToClose(true)
+									.title('Consulta marcada.')
+									.textContent('Consulta marcada com sucesso.')
+									.ariaLabel('Consulta marcada')
+									.ok('Ok!')
+									.targetEvent(ev)
+							);
+
+							$mdDialog.hide();
+						}, function (error) {
+							console.log(error);
+						});
+					};
+
+					$scope.cancel = function () {
+						$mdDialog.cancel();
+					};
+				},
+				templateUrl: '../views/dialog_add_appointment.html',
+				parent: angular.element(document.body),
+				targetEvent: ev,
+				clickOutsideToClose: true
+			});
+	}
 });
