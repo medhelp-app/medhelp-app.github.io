740c9fc34457080ef59de4f4a5d4756f37f02e7e
Paulo Menezes <paulo.hgmenezes@gmail.com>
2016-06-14 03:49:32 -0300

    Doctors in sidebar

--- js/controller/sideController.js
+++ js/controller/sideController.js
@@ -1,5 +1,11 @@
-app.controller('SideController', function($scope, $location, $cookies) {
+app.controller('SideController', function($scope, $location, $cookies, $http) {
 	$scope.type = $cookies.get('type');
+
+	var config = {
+		headers:  {
+			'x-access-token': $cookies.get('token')
+		}
+	};
 	
 	$scope.redirect = function (path) {
 		$location.path(path);
@@ -9,5 +15,30 @@ app.controller('SideController', function($scope, $location, $cookies) {
 		$cookies.remove('login');
 		$cookies.remove('user');
 		$location.path('/');
-	}
+	};
+
+	$http.get(API_URL + 'patients/' + $cookies.get('user') + '/appointments', config).then(function (data) {
+		$scope.doctors = [];
+		for (var i = 0; i < data.data.length; i++) {
+			var add = true;
+			for (var j = 0; j < $scope.doctors.length; j++) {
+				if (data.data[i].user._id == $scope.doctors[j]._id) {
+					add = false;
+					break;
+				}
+			};
+
+			if (add) {
+				$scope.doctors.push({
+					_id: data.data[i].user._id,
+					name: data.data[i].user.name,
+					profileImage: data.data[i].user.profileImage
+				})
+			}
+		};
+
+		console.log($scope.doctors);
+	}, function (error) {
+		console.log(error);
+	});
 })
