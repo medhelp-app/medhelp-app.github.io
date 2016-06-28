9aae314e636a2b65731f97ea21f4e4511dc08fb6
Guilherme Melo <guilhermemelo18@hotmail.com>
2016-06-28 01:40:17 -0300

    ...

--- js/controller/humanBodyCtrl.js
+++ js/controller/humanBodyCtrl.js
@@ -14,7 +14,7 @@ app.controller("HumanBodyCtrl", function($scope, $http, $location, $cookies, $md
 
 		$http.get(API_URL + 'patients/' + id + '/bodyparts', config).then(function (data) {
 			for (var i = 0; i < data.data.length; i++) {
-				data.data[i].problems.sort(function(a, b) {
+				data.data[i].problems.sort(function(a, b){
 					var x = a.severity == "High" ? 2 : (a.severity == "Medium" ? 1 : 0);
 					var y = b.severity == "High" ? 2 : (b.severity == "Medium" ? 1 : 0);
 					return x - y;
