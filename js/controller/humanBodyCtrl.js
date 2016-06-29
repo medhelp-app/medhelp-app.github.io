4bc9eed17b6d493694714bb850fecd21f2f2e261
Guilherme Melo <guilhermemelo18@hotmail.com>
2016-06-29 10:38:44 -0300

    ...

--- js/controller/humanBodyCtrl.js
+++ js/controller/humanBodyCtrl.js
@@ -131,6 +131,7 @@ app.controller("HumanBodyCtrl", function($scope, $http, $location, $cookies, $md
 
 					$scope.save = function (add) {
 
+						console.log(add);
 						if (!edit) {
 							$http.post(API_URL + 'patients/' + id + '/bodyparts', $scope.add, config).then(function (data) {
 								console.log(data);
