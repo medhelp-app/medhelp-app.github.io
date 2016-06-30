74404de1f287eb69e0921d649a37135cc25b5975
Guilherme Melo <guilhermemelo18@hotmail.com>
2016-06-30 20:40:22 -0300

    ...

--- js/controller/homeCtrl.js
+++ js/controller/homeCtrl.js
@@ -27,12 +27,7 @@ app.controller("HomeCtrl", function($scope, $http, $cookies, $location) {
 
 				FB.login(function(response) {
 				    if (response.authResponse) {
-				     console.log('Welcome!  Fetching your information.... ');
-				     FB.api('/me',{fields: 'name, email'}, function(response) {
-				       console.log('Good to see you, ' + response.name + '.');
-				       console.log(response);
-				       
-				     });
+				      cosole.log(response);
 				    } else {
 				     console.log('User cancelled login or did not fully authorize.');
 				    }
