9efc1049e397334a02b45e6f455daecd03274c84
Guilherme Melo <guilhermemelo18@hotmail.com>
2016-06-30 19:49:12 -0300

    .

--- js/controller/homeCtrl.js
+++ js/controller/homeCtrl.js
@@ -31,47 +31,12 @@ app.controller("HomeCtrl", function($scope, $http, $cookies, $location) {
 				     FB.api('/me',{fields: 'name, email'}, function(response) {
 				       console.log('Good to see you, ' + response.name + '.');
 				       console.log(response);
-
-				       var jsSha = new jsSHA(response.id);
-						var hash = jsSha.getHash("SHA-512", "HEX");
-
-						$http({
-						    method: "post",
-						    url: API_URL + "users/login",
-						    data: {
-						    	email: response.email,
-						    	password: hash
-						    }
-						}).success(function(data) {
-								$scope.user = { 
-									password: "",
-									email: ""
-								};
-
-								USER_ID = data.user._id;
-								TOKEN = data.token;
-
-								$scope.errostatus = false;
-								
-								$cookies.put('login', true);
-								$cookies.put('user', data.user._id);
-								$cookies.put('type', data.user.userType);
-								$cookies.put('token', data.token);
-							  	
-							  	//$location.path("inicio");
-							  	window.location = '#/inicio';
-				      			window.location.reload();
-							}).error(function(data) {
-								console.log("Deu Merda");
-								cadastroFaceBook(response);	
-
-							});
 								       
 				     });
 				    } else {
 				     console.log('User cancelled login or did not fully authorize.');
 				    }
-				},{scope: 'email'});
+				},{scope:'email'});
 
       		}
     	});
