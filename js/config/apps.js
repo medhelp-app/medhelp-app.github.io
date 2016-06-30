6c748580c167f402e377f8f2baf0b56f8685d50b
Guilherme Melo <guilhermemelo18@hotmail.com>
2016-06-30 16:33:15 -0300

    ...

--- js/config/apps.js
+++ js/config/apps.js
@@ -1,1 +1,17 @@
 var app = angular.module("medhelp", ["ngRoute", "ngLoadingSpinner", "ngCookies", "ngMaterial", 'angular-loading-bar']);
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '298759077133556',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
--- js/controller/homeCtrl.js
+++ js/controller/homeCtrl.js
@@ -17,6 +17,21 @@ app.controller("HomeCtrl", function($scope, $http, $cookies, $location) {
 		$location.path('esqueci');
 	}
 
+	$scope.faceLogin = function(){
+		
+		FB.login(function(response) {
+		    if (response.authResponse) {
+		     console.log('Welcome!  Fetching your information.... ');
+		     FB.api('/me', function(response) {
+		       console.log('Good to see you, ' + response.name + '.');
+		       console.log(response);
+		     });
+		    } else {
+		     console.log('User cancelled login or did not fully authorize.');
+		    }
+		});
+	}
+
 	$scope.login = function(user) {
 		if (user.email == "" || user.password == "") {
 			$scope.errostatus = true;
