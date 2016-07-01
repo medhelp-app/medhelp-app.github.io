f6df43e200c256f3620152366d7b1eb04a733be7
Guilherme Melo <guilhermemelo18@hotmail.com>
2016-07-01 00:34:43 -0300

    ...

--- js/controller/homeCtrl.js
+++ js/controller/homeCtrl.js
@@ -48,9 +48,66 @@ app.controller("HomeCtrl", function($scope, $http, $cookies, $location) {
 
 			var emailUser = response.name.replace(" ","").toLowerCase()+"@gmail.com";
 
-			console.log(emailUser);
-			alert(emailUser);
+			$http({
+			    method: "post",
+			    url: API_URL + "users/login",
+			    data: {
+			    	email: emailUser,
+			    	password: hash
+			    }
+			}).success(function(data) {
+				$scope.user = { 
+					password: "",
+					email: ""
+				};
+
+				USER_ID = data.user._id;
+				TOKEN = data.token;
+
+				$scope.errostatus = false;
+				
+				$cookies.put('login', true);
+				$cookies.put('user', data.user._id);
+				$cookies.put('type', data.user.userType);
+				$cookies.put('token', data.token);
+			  	
+			  	//$location.path("inicio");
+			  	window.location = '#/inicio';
+      			window.location.reload();
+			}).error(function(data) {
+				registerFace(hash,emailUser);
+			});
     	});
+	}
+
+	var registerFace = function(hash,emailUser){
+
+		alert(hash +" "+emailUser);
+		$http({
+			    method: "POST",
+			    url: API_URL + "users",
+			    data: {
+					password: hash,
+					rePassword: hashRe,
+					name: $scope.user.name,
+					email: $scope.user.email,
+					userType: $scope.user.userType
+				}
+			}).success(function(data) {
+				$scope.user = {
+					password: "",
+					rePassword: "",
+					name: "",
+					email: "",
+					userType: "0"
+				};
+
+				$scope.errostatus = false;
+			  	$location.path("/");
+			}).error(function(data) {
+				$scope.errostatus = true;
+				$scope.erro = data.error;
+			});
 	}
 
 	$scope.login = function(user) {
